813582e59e21ea125df551c20d57f7ee5d7fa20f
Tessa47 <t.zonneveld@campodesol.nl>
2024-11-13 12:35:21 +0100

    first commit

---
+++ R/mapToCDS.R
@@ -0,0 +1,182 @@
+#' mapToCDS
+#'
+#' Maps variants from genomic coordinates to CDS (coding sequence) positions. 
+#'
+#' @param object Input [`gdb`].
+#' @param gff Can be 1) a path to a valid gff- or gtf-file or 
+#' 2) a GenomicRanges::GRanges object. Generated, for example, using the [`rtracklayer::import`] function
+#' @param exonPadding Remap variants within `exonPadding` base pairs from the exon border to the border. Defaults to 12.
+#' @param output Optional output (.gz). If `NULL` remapped coordinates will be returned to session.
+#' @param gene_id Optional vector of gene ids to keep in the gff/gtf file.
+#' @param transcript_id Optional vector of transcript ids to keep in the gff/gtf file.
+#' @param biotype Optional vector of biotypes to keep in the gff/gtf file.
+#' @param verbose Should the function be verbose? Defaults to `TRUE`.
+#' @export
+setGeneric("mapToCDS",
+           function(object,
+                    gff,
+                    exonPadding = 12,
+                    output = NULL,
+                    gene_id = NULL,
+                    transcript_id = NULL,
+                    biotype = NULL,
+                    verbose = TRUE
+                    ) standardGeneric("mapToCDS"))
+
+setMethod("mapToCDS", 
+          signature = signature(object="gdb"),
+          definition=function(object,
+                              gff,
+                              exonPadding = 12,
+                              output = NULL,
+                              gene_id = NULL,
+                              transcript_id = NULL,
+                              biotype = NULL,
+                              verbose = TRUE
+                              
+          ) {
+            # load and filter gtf/gtf
+            if(is.character(gff)) {
+              gtf <- rtracklayer::import(gff)
+            } else if(is(gff, "GRanges")) {
+              gtf <- gff
+            } else {
+              stop("`gff` parameter should be either a path to a gff/gtf-file or a GRanges object")
+            }
+            
+            ### filter based on geneid
+            if (!is.null(gene_id)) {
+              gtf <- gtf[!is.na(gtf$gene_id) & gtf$gene_id %in% gene_id]
+            }
+            
+            if (!is.null(transcript_id)) {
+              gtf <- gtf[!is.na(gtf$transcript_id) & gtf$transcript_id %in% transcript_id]
+            }
+            
+            if (!is.null(biotype)) {
+              gtf <- gtf[(!is.na(gtf$transcript_biotype) & gtf$transcript_biotype %in% biotype) & (!is.na(gtf$gene_biotype) & gtf$gene_biotype %in% biotype)]
+            }
+            
+            exons <- gtf[gtf$type == "CDS"]
+            transcripts <- unique(exons$transcript_id)
+            GenomeInfoDb::seqlevelsStyle(exons)="NCBI"
+            if (is.null(output)) {
+              results <-  vector("list", length(transcripts))
+              names(results) <- transcripts
+            } else {
+              output <- gzcon(file(output,open='wb'))
+            }
+            
+            chrom <- unique(as.character(seqnames(exons)))
+            chrom_gdb <- RSQLite::dbGetQuery(object, "select CHROM from var_ranges")
+            if(any(grepl("chr", chrom_gdb$CHROM))) {
+              chrom_gdb <- gsub("chr", "", chrom_gdb$CHROM)
+              chrom_check <- TRUE
+            } else {
+              chrom_check <- FALSE
+            }
+            chrom <- intersect(chrom, chrom_gdb)
+            if(length(chrom)==0) {
+              stop("No overlap between chromosomes included in gtf and chromosomes included in gdb.")
+            }
+          
+            i <- 1
+            for(chr in chrom) {
+              if(verbose) message(sprintf("Mapping chromosome %s", chr))
+              transcripts_chr <- unique(exons[seqnames(exons)==chr]$transcript_id)
+              vars_chr <- unserialize(getAnno(object, "var_ranges", where = sprintf("CHROM = '%s%s'", if(chrom_check) "chr" else "", chr))$ranges[[1]])
+              GenomeInfoDb::seqlevelsStyle(vars_chr) <- "NCBI"
+              
+              for(transcript in transcripts_chr) {
+                ## subset exons 
+                exons_ <- sort(exons[exons$transcript_id %in% transcript])
+                strand <- unique(as.character(strand(exons_)))
+                
+                ## load variants based on chr,POS
+                ###  check if 'chr' needs to be added
+                add <- if(!is.null(exonPadding)) exonPadding else 0
+                vars <- subsetByOverlaps(vars_chr, exons_, maxgap = add)
+                if (length(vars) > 0) {
+                  ## gaps 
+                  deductionsPlus <- cumsum(width(GenomicRanges::gaps(exons_)))
+                  if (length(deductionsPlus) == 1) {
+                    deductionsMinus <- c(0)
+                  } else if (length(deductionsPlus) > 1) {
+                    deductionsMinus <- rev(c(0,cumsum(rev(width(GenomicRanges::gaps(exons_)[2:length(exons_)])))))
+                  }
+                  exons_$deductionsPlus <- deductionsPlus
+                  exons_$deductionsMinus <- deductionsMinus
+                  
+                  ## map variants to exons and add gaps
+                  overlaps <- GenomicRanges::findOverlaps(vars,exons_)
+                  
+                  if (!is.null(exonPadding)) {
+                    overlaps_padded <- GenomicRanges::findOverlaps(vars,exons_,maxgap = exonPadding)
+                    overlaps_padded <- overlaps_padded[!S4Vectors::queryHits(overlaps_padded) %in% S4Vectors::queryHits(overlaps)]
+                  }
+                  
+                  if ( length(overlaps_padded) > 0 ) {
+                    
+                    vars_padded <- vars[S4Vectors::queryHits(overlaps_padded)]
+                    vars_padded$distStart <- start(exons_[S4Vectors::subjectHits(overlaps_padded)]) - end(vars_padded) - 1
+                    vars_padded$distEnd <- start(vars_padded) - end(exons_[S4Vectors::subjectHits(overlaps_padded)]) - 1 
+                    vars_padded$POS_updated = ifelse(vars_padded$distStart >= 0 & vars_padded$distStart <= exonPadding,
+                                                     start(vars_padded) + vars_padded$distStart + 1,
+                                                     start(vars_padded) - vars_padded$distEnd - 1
+                    )
+                    ranges(vars_padded) <- IRanges::IRanges(
+                      start = vars_padded$POS_updated,
+                      end = vars_padded$POS_updated
+                    )
+                    vars_padded$distStart <- vars_padded$distEnd <- vars_padded$POS_updated <- NULL
+                    mcols(vars_padded) <- cbind(mcols(vars_padded), mcols(exons_[S4Vectors::subjectHits(overlaps_padded)])[,c("gene_id", "transcript_id", "deductionsPlus", "deductionsMinus")])
+                    vars_padded$padded = TRUE
+                  }
+                  
+                  if ( length(overlaps) > 0 ) {
+                    # generate new variant positions 
+                    vars <- vars[S4Vectors::queryHits(overlaps)]
+                    mcols(vars) <- cbind(mcols(vars), mcols(exons_[S4Vectors::subjectHits(overlaps)])[,c("gene_id", "transcript_id", "deductionsPlus", "deductionsMinus")])
+                    vars$padded <- FALSE
+                  } else {
+                    vars <- vars[0,]
+                  }
+                  
+                  if (length(overlaps) > 0 && length(overlaps_padded) > 0) {
+                    vars <- sort(c(vars,vars_padded))
+                  }  else if (length(overlaps_padded) > 0 && length(overlaps) == 0 ) {
+                    vars <- vars_padded
+                  }
+                  
+                  if (length(vars) > 0) {
+                    if (strand == "+") {
+                      mcols(vars)[["cdsPOS"]] <- start(vars) - vars$deductionsPlus
+                    } else if (strand == "-") {
+                      mcols(vars)[["cdsPOS"]] <- rep(max(end(exons_)) + 1, length(vars)) - start(vars) - vars$deductionsMinus
+                    }
+                    names(vars) <- NULL
+                    vars <- as.data.frame(vars)[,c("VAR_id", "seqnames", "start","gene_id", "transcript_id", "cdsPOS", "padded")]
+                    colnames(vars) <- c("VAR_id", "CHROM", "POS", "gene_id", "transcript_id", "cdsPOS", "padded")
+                    if(!is.null(output)) {
+                      if(i == 1) {
+                        write.table(vars, file = output, append = FALSE, row.names = FALSE, col.names = TRUE, quote = FALSE, sep = "\t")
+                      } else {
+                        write.table(vars, file = output, append = TRUE, row.names = FALSE, col.names = FALSE, quote = FALSE, sep = "\t")
+                      }
+                    } else {
+                      results[[transcript]] <- vars
+                    }
+                  }
+                }
+                i <- i+1
+              }
+            }
+            if(!is.null(output)) {
+              close(output)
+            } else {
+              results <- do.call(rbind,results)
+              rownames(results) <- NULL
+              results
+            }
+          }
+)
---
+++ R/spatialClust.R
@@ -0,0 +1,161 @@
+#' @rdname spatialClust
+#' @usage NULL
+#' @export
+setMethod("spatialClust", signature="gdb",
+          definition = function(object,output,varSetName,unitTable,unitName,windowSize,overlap,intersection=NULL,where=NULL,weightName="1", posField="POS",minTry=5,warning=TRUE)
+          {
+            if (length(windowSize)!=length(overlap)){stop(sprintf("Number of provided window sizes (%s) does not match number of provided overlaps (%s)",
+                                                                  paste(windowSize,collapse=","),paste(overlap,collapse=",")))}
+            output=gzfile(output,"w")
+            query=sprintf("select distinct %s as unit, VAR_id, %s as weight, %s as POS from %s", unitName, weightName, posField, unitTable)
+            
+            if (!is.null(intersection)){intersection=unlist(strsplit(intersection,split=","))}
+            for (i in intersection)
+            {
+              query=sprintf("%s inner join %s using (VAR_id)",query,i)
+            }
+            if (!is.null(where)){query <- sprintf("%s where %s",query, where)}
+            query=sprintf("select unit, group_concat(VAR_id) as VAR_id, group_concat(weight) as weight, '%s' as varSetName, group_concat(POS) as POS from (%s) x group by unit",varSetName, query)
+            
+            metadata <- list(
+              rvatVersion = as.character(packageVersion("rvat")),
+              gdbId = getGdbId(object),
+              genomeBuild = getGenomeBuild(object),
+              creationDate = as.character(round(Sys.time(), units = "secs"))
+            )
+            .write_rvat_header(filetype = "varSetFile", 
+                               metadata = metadata, 
+                               con = output)
+            
+            handle=RSQLite::dbSendQuery(object,query)
+            while (!RSQLite::dbHasCompleted(handle))
+            {
+              varSet=RSQLite::dbFetch(handle,n=1)
+              runDistanceCluster(varSet=varSet,posField=posField,windowSize=windowSize,overlap=overlap,output=output,minTry=minTry,warning=warning)
+            }
+            RSQLite::dbClearResult(handle)
+            close(output)
+          }
+)
+
+
+# runDistanceCluster
+# Parser to handle input/ output of variant data for distanceCluster
+runDistanceCluster=function(varSet,posField,windowSize,overlap,output,minTry=5,warning=TRUE)
+{
+  unit=varSet$unit
+  varSetName=varSet$varSetName
+  varSet=data.frame(VAR_id=unlist(strsplit(varSet$VAR_id,",")),
+                    weight=unlist(strsplit(varSet$weight,",")),
+                    POS=unlist(strsplit(as.character(varSet$POS),",")),
+                    stringsAsFactors=FALSE
+  )
+  
+  if (nrow(varSet) < minTry ) {
+    if (warning) warning(sprintf("varSet contains fewer than %s variants ('minTry' parameter), all variants are returned as a single cluster.", minTry))
+    for (wi in 1:length(windowSize)) {
+      varSeti=paste(paste(unit,'0',sep="_"),paste(varSet$VAR_id,collapse=","),paste(varSet$weight,collapse=","),paste(varSetName,windowSize[wi],overlap[wi],sep="_"),sep="|")
+      write(varSeti,output,append=TRUE)
+    }
+    return(NULL)
+  }
+  
+  if (posField=="HGVSc") {
+    varSet$POS=as.integer(stringr::str_replace(varSet$POS,"c.([0-9]*).*","\\1"))
+  } else {
+    varSet$POS=as.integer(varSet$POS)
+  }
+  
+  for (wi in 1:length(windowSize))
+  {
+    parts=distanceCluster(pos=varSet$POS,names=varSet$VAR_id,windowSize=windowSize[wi],overlap=overlap[wi],return_what="names", write_output=FALSE, path_output=NA,warning=warning)
+    for (part in names(parts))
+    {
+      varSeti=apply(varSet[varSet$VAR_id %in% parts[[part]],],2,paste,collapse=",")
+      varSeti=paste(paste(unit,part,sep="_"),varSeti["VAR_id"],varSeti["weight"],paste(varSetName,windowSize[wi],overlap[wi],sep="_"),sep="|")
+      write(varSeti,output,append=TRUE)
+    }
+  }
+}
+
+# distanceCluster
+# Adapted from https://github.com/heidefier/cluster_wgs_data (Fier, GenetEpidemiol, 2017). 
+# Partitions a supplied set of variant until the mean distance between variants equals the median (as would be expected for a homogeneous poisson process).
+
+distanceCluster<-function(pos,names,windowSize=100,overlap=50,return_what="names", write_output=TRUE, path_output=NA,warning=TRUE){
+  
+  if (!(return_what %in% c("names","positions"))){
+    stop('Please enter "names" OR "positions" as argument for return_what')
+  }
+  
+  if (write_output & is.na(path_output)){
+    stop('Please specify a valid output name in path_output','\n')
+  }
+  
+  b<-as.numeric(pos)    
+  comb<-data.frame(b = b, names = names,stringsAsFactors=FALSE)
+  comb<-comb[order(b),]
+  b<-as.numeric(comb[,1])
+  names<-comb[,2]
+  bdiff<-diff(b)
+  
+  # calculate lambda under the assumption that distances follow exp distribution
+  
+  lambda<-log(2)/median(bdiff)
+  
+  #observed mean values in the windows
+  
+  obsMeanWindow <- zoo::rollapply(bdiff,windowSize,mean,by=overlap,align="left")
+  if (length(obsMeanWindow) == 0) {
+    if(warning) warning("No valid partitioning for specified parameters, all variants are returned as a single cluster.")
+    return(list("0" = names))
+  }
+  
+  #theoretical mean value given medians in the windows
+  ws<-as.numeric(windowSize)
+  sh<-as.numeric(overlap)
+  
+  theoMeanWindow <-zoo::rollapply(bdiff,ws,function(x) 1/(log(2)/median(x)),by=sh,align="left")
+  
+  #actual positions
+  raPositions <- zoo::rollapply(b,ws+1,function(x) x,by=sh,align="left")
+  
+  maximas<-vector("list",length(obsMeanWindow))
+  for (i in 1:length(obsMeanWindow)){
+    if (obsMeanWindow[i] <= theoMeanWindow[i]) {
+      maximas[[i]]<-NA
+    } else {
+      ord<-order(diff(raPositions[i,]),decreasing=TRUE)
+      j<-1
+      excl<-mean(diff(raPositions[i,])[-c(ord[1])])
+      while (theoMeanWindow[i]<excl){
+        j<-j+1
+        excl<-mean(diff(raPositions[i,])[-c(ord[1:j])]) #The largest distances are removed until the median-informed theoretical means are larger than the mean of the edited window
+      }
+      j<-j-1
+      maximas[[i]]<-raPositions[i,][c(ord[1:j])+1] 
+    }
+  }
+  maxima <- unlist(maximas)
+  maxima <- maxima[!is.na(maxima)]
+  maximaf <- sort(unique(maxima))
+  
+  if (length(maximaf) == 0) {
+    if(warning) warning("No valid partitioning for specified parameters, all variants are returned as a single cluster.")
+    return(list("0" = names))
+  }
+  
+  if (!is.na(path_output)) file.create(path_output)
+  if (return_what=="positions") {
+    lo_final<-split(b,findInterval(b,maximaf))
+    ll<-as.numeric(lapply(lo_final,length))
+    if (return_what=="positions" && write_output) lapply(lo_final, write, path_output, append=TRUE, ncolumns=max(ll) )
+  } else {
+    lo_final<-split(names,findInterval(b,maximaf))
+    ll<-as.numeric(lapply(lo_final,length))
+    if (return_what=="names" && write_output)  lapply(lo_final, write, path_output, append=TRUE, ncolumns=max(ll) )
+  }
+  
+  return(lo_final)
+  
+}
